fbc2578a487bbaadcbacd20534308b688c641f07
66112 <1031802507@qq.com>
2019-02-14 12:41:32 +0800

    char_date

---
+++ first.sh
@@ -0,0 +1,33 @@
+#!/bin/bash 
+
+#mydate=`date +%Y-%m-%d`
+mydate=$(date +%Y-%m-%d)
+echo $mydate
+
+
+#echo ${#str}  #获取字符串长度
+
+#x=100;
+#echo $x
+#unset x     #删除一个变量,但不能删除只读变量
+#echo $x
+
+#readonly x=100  #变量只读
+#echo $x
+
+#myint=123
+#myint = 123     #myint后边不能带空格,不然会解释成命令
+#myfloat=3.14
+#mystr1="hello shell"
+#mystr2=",hello linux"
+#
+#mychar='A'
+#echo $myint        #打印该变量
+#echo $myfloat       
+#echo $mychar       
+#echo $mystr1$mystr2   #拼接字符串
+#echo ${myint}aaaa  #可以将变量名分开 
+
+#pwd
+#cd ..
+#pwd
