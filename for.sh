037bde87a76d1373904d46ad5c2b548cdfde9637
66112 <1031802507@qq.com>
2019-02-17 09:15:46 +0800

    for循环

---
+++ for.sh
@@ -0,0 +1,46 @@
+#!/bin/bash 
+
+#until false
+#do 
+#    echo "hello"
+#done 
+
+#while :
+#do 
+#    echo "hello word"
+#done
+
+#while ! false         # 必须要有空格
+#do 
+#    echo "hello word"
+#done
+
+#until [ $i -gt 10 ]  #直到 i 大于 10 才不会再循环
+#do 
+#    echo $i
+#    let i++
+#done
+#for i in {1..10}{a..b}
+#do 
+#    echo $i
+#done
+
+#for i in 1 3 5 7 a b c d
+#do 
+#    echo $i
+#done 
+
+#i=0;while [ $i -lt 10 ];do echo $i;let i++;done
+#i=0
+#while [ $i -lt 10 ]
+#do 
+#    echo $i
+#    let i++
+#    #((i++))
+#done
+
+#for ((i=0; i<10; i++ ))    # 可以做类C风格的事情
+#do 
+#    echo $i
+#done
+
